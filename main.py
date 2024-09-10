f73b712a22ae5a26c274048fa402436deb5d30f0
VitaliiKit <welld-94@mail.ru>
2024-09-10 16:30:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+'1st program'
+print (9** 0.5 * 5)
+'2st program'
+print (9.99 > 9.98 and 1000 != 1000.1)
+'3st program'
+print(2 * 2 + 2 == 2 * (2 + 2))
+'4st program'
+a = '123.456'
+b = (float ('123.456'))
+print(b)
+print(b*10)
+c= (b*10)
+print(int(c%10))
